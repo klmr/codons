ff7f24b8ad466d39a64cab7d7629a2e7002ab1d6
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-02-02 23:52:44 +0000

    Load sensible knitr defaults

---
+++ scripts/knit.r
@@ -0,0 +1,53 @@
+library = function (...) suppressMessages(base::library(...))
+assign('library', library, globalenv())
+
+library(knitr)
+library(modules)
+
+options(stringsAsFactors = FALSE,
+        import.path = file.path(Sys.getenv('HOME'), 'Projects/R'))
+
+#opts_chunk$set(cache = TRUE)
+
+# Pretty-print tables
+
+library(pander)
+
+panderOptions('table.split.table', Inf)
+panderOptions('table.alignment.default',
+              function (df) ifelse(sapply(df, is.numeric), 'right', 'left'))
+panderOptions('table.alignment.rownames', 'left')
+
+# Enable automatic table reformatting.
+opts_chunk$set(render = function (object, ...) {
+    if (is.data.frame(object) ||
+        is.matrix(object) ||
+        is.tbl_df(object))
+        pander(object, style = 'rmarkdown')
+    else if (isS4(object))
+        show(object)
+    else
+        print(object)
+})
+
+# Helpers for dplyr tables
+
+is.tbl_df = function (x)
+    'tbl_df' %in% class(x)
+
+pander.tbl_df = function (x, ...)
+    pander(trunc_mat(x), ...)
+
+# Copied from dplyr:::print.trunc_mat
+pander.trunc_mat = function (x, ...) {
+    if (! is.null(x$table))
+        pander(x$table, ...)
+
+    if (length(x$extra) > 0) {
+        var_types = paste0(names(x$extra), ' (', x$extra, ')', collapse = ', ')
+        pander(dplyr:::wrap('Variables not shown: ', var_types))
+    }
+}
+
+# Disable code re-formatting.
+opts_chunk$set(tidy = FALSE)
