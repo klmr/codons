c6016e31f5620fcb918ea36cdeecef5b81829358
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-03-11 14:41:40 +0000

    Prefix config with data type

--- config_human.r
+++ config_human.r
@@ -1,7 +1,7 @@
 species = 'human'
-counts = './data/trna-counts-hs38.tsv'
-annotation = './data/Homo_sapiens.GRCh38.dna.primary_assembly.trna.filtered.out'
-design = './data/libraries-trna-chip-hs38.tsv'
+trna_counts = './data/trna-counts-hs38.tsv'
+trna_annotation = './data/Homo_sapiens.GRCh38.dna.primary_assembly.trna.filtered.out'
+trna_design = './data/libraries-trna-chip-hs38.tsv'
 norm_counts = './results/trna-norm-counts-hs38.tsv'
 contrasts = list(c('liver', 'Huh7'),
                  c('liver', 'HepG2'),
--- config_mouse.r
+++ config_mouse.r
@@ -1,7 +1,8 @@
 species = 'mouse'
-counts = './data/trna-counts-mm10.tsv'
-annotation = './data/tRNA_Scan_Mus_musculus.GRCm38.69_301014.filtered.out'
-design = './data/libraries-trna-chip-mm10.tsv'
+trna_counts = './data/trna-counts-mm10.tsv'
+mrna_counts = './data/rnaseq-counts-mm10.tsv'
+trna_annotation = './data/tRNA_Scan_Mus_musculus.GRCm38.69_301014.filtered.out'
+trna_design = './data/libraries-trna-chip-mm10.tsv'
 norm_counts = './results/trna-norm-counts-mm10.tsv'
 contrasts = list(c('liver', 'Hepa1-6'),
                  c('liver', 'Hepa1c1c7'),
