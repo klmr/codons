99d0f3b4b9becbe36b3896f9bd2b15cf0acf6233
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-03-12 15:10:05 +0000

    Load data and filter unexpressed genes

--- config_mouse.r
+++ config_mouse.r
@@ -2,6 +2,7 @@ species = 'mouse'
 trna_counts = './data/trna-counts-mm10.tsv'
 mrna_counts = './data/rnaseq-counts-mm10.tsv'
 trna_annotation = './data/tRNA_Scan_Mus_musculus.GRCm38.69_301014.filtered.out'
+mrna_annotation = './data/Mus_musculus.GRCm38.75.gene_annot.tsv'
 trna_design = './data/libraries-trna-chip-mm10.tsv'
 norm_counts = './results/trna-norm-counts-mm10.tsv'
 contrasts = list(c('liver', 'Hepa1-6'),
