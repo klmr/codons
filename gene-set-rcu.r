5c48280d74c3ac4057dee8f0b39cdfdfbb0bea7d
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-05-28 23:16:02 +0100

    Calculate RCU bias

--- gene-set-rcu.r
+++ gene-set-rcu.r
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 options(stringsAsFactors = FALSE)
 library(modules) # Needed due to bug #44 in modules.
+library(dplyr)
 sys = modules::import('scripts/sys')
 
 force_arg = function (pos, msg, error_code = 1) {
@@ -10,6 +11,13 @@ force_arg = function (pos, msg, error_code = 1) {
     arg
 }
 
+rcu = function (codon_usage)
+    codon_usage %>%
+    group_by(AA, Codon) %>%
+    summarize(Count = sum(Count)) %>%
+    mutate(Ratio = Count / sum(Count)) %>%
+    ungroup()
+
 sys$run({
     codon_usage_file = force_arg(1, 'No codon usage filename provided')
     gene_set_file = force_arg(2, 'No gene set filename provided')
@@ -18,5 +26,10 @@ sys$run({
     codon_usage = readRDS(codon_usage_file)
     gene_set = readLines(gene_set_file)
 
+    background_rcu = rcu(codon_usage)
+    rcu = rcu(filter(codon_usage, Gene %in% gene_set))
+
+    correlation = cor(background_rcu$Ratio, rcu$Ratio)
+    writeLines(sprintf('%f', correlation), outfile)
     NULL
 })
