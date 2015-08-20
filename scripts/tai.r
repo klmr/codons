33f497d488df9917ee8d992f6ee0ed93badd917c
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-08-20 15:38:20 +0100

    Implement calculation of Ws

--- scripts/tai.r
+++ scripts/tai.r
@@ -31,7 +31,27 @@ rc_anticodons = c('TTT', 'TTC', 'TTA', 'TTG',
 met_codon = match('ATG', rc_anticodons)
 stop_codons = match(c('TAA', 'TAG', 'TGA'), rc_anticodons)
 
-ws = function () {
+ws = function (counts, s = get_s(species), species) {
+    counts = counts[rc_anticodons]
+    counts[is.na(counts)] = 0
+    p = 1 - s
+    w = vector('numeric', length(counts))
+
+    for (i in seq(1, length(counts), by = 4)) {
+        w[i]     = p[1] * counts[i]     + p[5] * counts[i + 1]
+        w[i + 1] = p[2] * counts[i + 1] + p[6] * counts[i]
+        w[i + 2] = p[3] * counts[i + 2] + p[7] * counts[i]
+        w[i + 3] = p[4] * counts[i + 3] + p[8] * counts[i + 2]
+    }
+
+    w[met_codon] = p[4] * counts[met_codon]
+    w = w[-stop_codons]
+    w_rel = w / max(w)
+    nonzero_w = w_rel[w != 0]
+    if (length(nonzero_w) != length(w_rel))
+        w_rel[w_rel == 0] = exp(sum(log(nonzero_w)) / length(nonzero_w))
+
+    w_rel
 }
 
 tai = function (codon_counts, w, lengths) {
