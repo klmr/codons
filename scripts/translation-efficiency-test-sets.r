bc53daad4191cea49be2ef4fed161babcb37f2b1
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-11-18 15:06:51 +0000

    Rename “contrast” to “relation”
    The term “contrast” was overloaded in the analysis: one usage referred
    to the comparison of the mRNA transcriptome of a cellular condition with
    the tRNA transcriptome of another (or the same) cellular condition. The
    other usage referred to a differential expression contrast of mRNA
    genes, to determine the transcriptome gene set under investigation.
    
    This commit resolves the confusion by renaming the cell types of the
    mRNA–tRNA comparison into “relation”, and reserving “contrast” for the
    condition names in a differential expression analysis.

--- scripts/translation-efficiency-test-sets.r
+++ scripts/translation-efficiency-test-sets.r
@@ -1,12 +1,12 @@
-define_contrasts = function (config) {
+define_relations = function (config) {
     all_celltypes = unique(data$mrna_design(config)$Celltype)
     healthy_celltypes = intersect(all_celltypes, c('Liver-Adult', 'E15.5'))
     cancer_celltypes = setdiff(all_celltypes, healthy_celltypes)
-    all_contrasts = expand.grid(Codon = unique(all_celltypes),
+    all_relations = expand.grid(Codon = unique(all_celltypes),
                                 Anti = unique(all_celltypes),
                                 stringsAsFactors = FALSE)
-    matching_contrasts = filter(all_contrasts, Codon == Anti)
-    mismatching_contrasts = all_contrasts %>%
+    matching_relations = filter(all_relations, Codon == Anti)
+    mismatching_relations = all_relations %>%
         filter(Codon != Anti,
                Codon %in% healthy_celltypes |
                Anti %in% healthy_celltypes)
@@ -15,10 +15,10 @@ define_contrasts = function (config) {
     invisible(lapply(ls(), function (n) assign(n, get(n), globalenv)))
 }
 
-#' Get all pairwise replicate library identifiers for a given contrast.
-expand_contrast = function (contrast) {
-    mrna_samples = filter(data$mrna_design(config), Celltype == contrast[1])$DO
-    trna_samples = filter(data$trna_design(config), Celltype == contrast[2])$DO
+#' Get all pairwise replicate library identifiers for a given relation.
+expand_relation = function (relation) {
+    mrna_samples = filter(data$mrna_design(config), Celltype == relation[1])$DO
+    trna_samples = filter(data$trna_design(config), Celltype == relation[2])$DO
     as.data.frame(t(expand.grid(mrna_samples, trna_samples)))
 }
 
