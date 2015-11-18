841aef5e9879c55ed0b5ce0edbd569be5efdc565
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-11-18 15:10:51 +0000

    Add functions to generate test gene sets

--- scripts/translation-efficiency-test-sets.r
+++ scripts/translation-efficiency-test-sets.r
@@ -22,3 +22,29 @@ expand_relation = function (relation) {
     as.data.frame(t(expand.grid(mrna_samples, trna_samples)))
 }
 
+#' Create gene sets of a given condition
+#'
+#' @param condition name of the condition
+#' @return Returns a set of sets of gene IDs.
+whole_transcriptome = function (condition)
+    list(unique(cu$Gene))
+
+#' 
+#' \code{upregulated_genes} returns the set of sets of upregulated gene IDs for
+#' each contrast involving \code{condition}.
+#' @return \code{upregulated_genes} returns the set of sets of upregulated
+#' gene IDs for each contrast involving \code{condition}.
+#' @rdname whole_transcriptome
+upregulated_genes = local({
+    # FIXME: This is a mess, do proper filtering. My brain is fried.
+    upregulated_genes = readRDS(sprintf('results/de/up-%s.rds', config$species))
+    de_contrast_selection = names(upregulated_genes) %>%
+        {grep('Liver-Adult|E15\\.5', .)}
+    upregulated_genes = upregulated_genes[de_contrast_selection]
+
+    function (condition) {
+        # The genes are upregulated in the second condition of the contrast.
+        indices = grep(sprintf('/%s', condition), names(upregulated_genes))
+        upregulated_genes[indices]
+    }
+})
